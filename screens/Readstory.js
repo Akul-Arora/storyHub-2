e72fc25ab88ce16914e06fc6435959c8007082e2
anshika4 <72254585+anshika4@users.noreply.github.com>
2021-02-23 17:01:45 +0530

    Update Readstory.js

--- screens/Readstory.js
+++ screens/Readstory.js
@@ -1,16 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>ReadStory</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({import React from 'react';
 import {Text, View ,StyleSheet} from 'react-native';
 
 export default class ReadScreen extends React.Component {
@@ -42,12 +30,4 @@ backgroundColor:'#ffb6c1',
 textAlign:'center',
 fontSize :30,
 },
-});
-
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
